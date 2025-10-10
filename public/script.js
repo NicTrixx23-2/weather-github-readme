6e90c00f6394c3984e7ad93a411fd07b799fad0c
Nico <nico.scholl@loewenrot.de>
2025-10-10 08:52:49 +0200

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,10 @@
+function generate() {
+  const loc = encodeURIComponent(document.getElementById("location").value.trim());
+  const url = `${window.location.origin}/api/weather?location=${loc}`;
+  document.getElementById("result").innerHTML = `
+    <p>Markdown:</p>
+    <code>![Wetter](${url})</code>
+    <p>Vorschau:</p>
+    <img src="${url}" alt="Wetter Widget" style="margin-top:10px; border:1px solid #ccc;" />
+  `;
+}
